452daac20f71c5217d4310dc7bf9b71e20e08b8c
Corum Gold <goldcorum@gmail.com>
2021-06-12 11:30:00 -0400

    remove for/of loop and replace with a regular for loop. Needed help from the video, not sure why my for/of loop didn't work.

--- todos.js
+++ todos.js
@@ -3,21 +3,21 @@ let input = prompt("What would you like me to do?");
 
 while (input !== 'quit' && input !== 'q') {
     if (input === 'list') {
-        for (let todo of todos) {
-            console.log('************');
-            console.log(`${todos.indexOf(todo)}: ${todo}`);
-            console.log('************');
+        console.log('************');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`)
         }
+        // for (let todo of todos) {
+        //     console.log(`${todos.indexOf(todo)}: ${todo}`);
+        // }
+        console.log('************');
     }
     else if (input === 'new') {
         const item = prompt('What would you like to add?');
         todos.push(item);
         console.log(`You have added ${item} to your list`)
     }
-    else if (input === 'delete') {
-        let itemToDelete = prompt('What would you like to delete?');
-    } else {
-        let input = prompt('Please enter a valid command.');
-    } input = prompt ('What would you like me to do?')
+
+    input = prompt('What would you like me to do?')
 
 } console.log('You quit the app')
