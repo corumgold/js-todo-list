a530c8f4de8d3621a0bf05abb350761f8cca6cfe
Corum Gold <goldcorum@gmail.com>
2021-06-12 11:16:49 -0400

    add second 'what would you like me to do?' at end of loop. Needed help from the video

--- todos.js
+++ todos.js
@@ -2,23 +2,22 @@ const todos = ['fart', 'farting', 'fartiest'];
 let input = prompt("What would you like me to do?");
 
 while (input !== 'quit' && input !== 'q') {
-    let input = prompt("Is there anything else you would like me to do?")
-    if (input == 'list') {
+    if (input === 'list') {
         for (let todo of todos) {
             console.log('************');
             console.log(`${todos.indexOf(todo)}: ${todo}`);
             console.log('************');
         }
     }
-    else if (input == 'new') {
+    else if (input === 'new') {
         const item = prompt('What would you like to add?');
         todos.push(item);
         console.log(`You have added ${item} to your list`)
     }
-    else if (input == 'delete') {
+    else if (input === 'delete') {
         let itemToDelete = prompt('What would you like to delete?');
     } else {
         let input = prompt('Please enter a valid command.');
-    }
+    } input = prompt ('What would you like me to do?')
 
 } console.log('You quit the app')
