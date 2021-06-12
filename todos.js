22c141a38d1655017a5cd91ecff7912e5fe152a4
Corum Gold <goldcorum@gmail.com>
2021-06-12 11:05:23 -0400

    add 'q' to quit the app

--- todos.js
+++ todos.js
@@ -1,21 +1,23 @@
 const todos = ['fart', 'farting', 'fartiest']
 let input = prompt("What would you like me to do?")
 
-// while (input !== 'quit') {
+while (input !== 'quit' && input !== 'q') {
+    let input = prompt("Is there anything else you would like me to do?")
     if (input == 'new') {
         let item = prompt('What would you like to add?');
         todos.push(item);
         console.log(`You have added ${item} to your list`)
     } else if (input == 'list') {
-        for (let todo of todos){
+        for (let todo of todos) {
             console.log(`${todos.indexOf(todo)}: ${todo}`);
         }
     } else if (input == 'delete') {
         let itemToDelete = prompt('What would you like to delete?');
     } else {
-        let input = prompt('Please enter a valid command.')
+        let input = prompt('Please enter a valid command.');
     }
-// }
+
+}
 
 
 console.log('You quit the app')
