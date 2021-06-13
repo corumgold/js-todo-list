9c83b75c70e92e116ca289532b2f5669ac18c947
Corum Gold <goldcorum@gmail.com>
2021-06-13 11:52:29 -0400

    add delete feature

--- todos.js
+++ todos.js
@@ -2,6 +2,7 @@ const todos = ['fart', 'farting', 'fartiest'];
 let input = prompt("What would you like me to do?");
 
 while (input !== 'quit' && input !== 'q') {
+    input.toLowerCase();
     if (input === 'list') {
         console.log('************');
         for (let i = 0; i < todos.length; i++) {
@@ -16,6 +17,11 @@ while (input !== 'quit' && input !== 'q') {
         const item = prompt('What would you like to add?');
         todos.push(item);
         console.log(`You have added ${item} to your list`)
+    } 
+    else if (input === 'delete'){
+        const index = prompt('What is the index (number) of the item to delete?');
+        const deleted = todos.splice(index, 1);
+        console.log(`OK! Deleted ${deleted[0]} successfully!`)
     }
 
     input = prompt('What would you like me to do?')
